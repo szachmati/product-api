323bd7da4c24d15e9c83f8c6bf0325afa1d8334a
szachMati <mateusz.hawryluk30@gmail.com>
2022-08-30 21:31:53 +0200

    [Micronaut] Use spotify hamcrest-pojo matchers instead of io.github.marmer.testutils

--- micronaut/src/test/java/pl/wit/shop/product/domain/ProductRepositoryTest.java
+++ micronaut/src/test/java/pl/wit/shop/product/domain/ProductRepositoryTest.java
@@ -8,9 +8,12 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import pl.wit.shop.product.test.data.ProductTestDataIdentifiers;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import static com.spotify.hamcrest.pojo.IsPojo.pojo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static pl.wit.shop.product.domain.ProductBuilder.aFirstHomeProduct;
 import static pl.wit.shop.product.domain.ProductBuilder.aMonitorProduct;
@@ -29,30 +32,41 @@ class ProductRepositoryTest implements ProductTestDataIdentifiers {
 
     @Test
     void findAllProductsInCategory_shouldReturnPageWithHomeProductsSortedByPriceDesc() {
-        ProductCategory homeCategory = productCategoryRepository.save(aHomeProductCategory().build());
-        ProductCategory electronicsCategory = productCategoryRepository.save(anElectronicsProductCategory().build());
+        final ProductCategory homeCategory = productCategoryRepository.save(aHomeProductCategory().build());
+        final ProductCategory electronicsCategory = productCategoryRepository.save(anElectronicsProductCategory().build());
+        final Product firstHomeProduct = aFirstHomeProduct()
+                .withPrice(new BigDecimal("11.99"))
+                .withCategory(homeCategory)
+                .build();
+        final Product secondHomeProduct = aSecondHomeProduct()
+                .withCategory(homeCategory)
+                .withPrice(new BigDecimal("20.30"))
+                .build();
         productRepository.saveAll(List.of(
-                aFirstHomeProduct().withCategory(homeCategory).build(),
-                aSecondHomeProduct().withCategory(homeCategory).build(),
+                firstHomeProduct,
+                secondHomeProduct,
                 aMonitorProduct().withCategory(electronicsCategory).build()
         ));
 
-
-        Page<Product> products = productRepository.findAllProductsInCategory("HOME",
+        Page<Product> result = productRepository.findAllProductsInCategory("HOME",
                 Pageable.from(0, 5, Sort.of(Sort.Order.desc("price")))
         );
 
-        assertThat(products.getTotalSize(), is(2L));
-        //TODO
-//        assertThat(products.getContent(), contains(
-//               ProductMatcher.isProduct()
-//                        .withUuid(PRODUCT_2_UUID)
-//                        .withCategory(isProductCategory().withName("HOME"))
-//                        .withPrice(new BigDecimal("30.87")),
-//                isProduct()
-//                        .withUuid(PRODUCT_1_UUID)
-//                        .withCategory(isProductCategory().withName("HOME"))
-//                        .withPrice(BigDecimal.ONE)
-//        ));
+        assertThat(result.getContent(), contains(
+                pojo(Product.class)
+                        .withProperty("name", is("Second home product"))
+                        .withProperty("price", is(new BigDecimal("20.30")))
+                        .where("getCategory",
+                                pojo(ProductCategory.class)
+                                        .withProperty("name", is("HOME"))
+                        ),
+                pojo(Product.class)
+                        .withProperty("name", is("Home product"))
+                        .withProperty("price", is(new BigDecimal("11.99")))
+                        .where("getCategory",
+                                pojo(ProductCategory.class)
+                                        .withProperty("name", is("HOME"))
+                        )
+        ));
     }
 }
