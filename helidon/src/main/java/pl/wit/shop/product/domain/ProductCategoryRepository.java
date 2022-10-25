ba10a268db13599b79d2c3942f507d5d0ed9c2b8
szachMati <mateusz.hawryluk30@gmail.com>
2022-10-25 21:22:18 +0200

    [Helidon] Add create product method

--- helidon/src/main/java/pl/wit/shop/product/domain/ProductCategoryRepository.java
+++ helidon/src/main/java/pl/wit/shop/product/domain/ProductCategoryRepository.java
@@ -3,9 +3,16 @@ package pl.wit.shop.product.domain;
 import pl.wit.shop.common.repository.BaseRepository;
 import pl.wit.shop.shared.exception.NotFoundException;
 
+import java.util.function.Supplier;
+
 public interface ProductCategoryRepository extends BaseRepository<ProductCategory> {
 
     ProductCategory getByName(String name);
+
+    @Override
+    default Supplier<NotFoundException> notFoundException(String cause) {
+        return () -> new ProductCategoryNotFoundException(cause);
+    }
 
     class ProductCategoryNotFoundException extends NotFoundException {
        public ProductCategoryNotFoundException(String cause) {
--- helidon/src/main/java/pl/wit/shop/product/domain/ProductCategoryRepositoryImpl.java
+++ helidon/src/main/java/pl/wit/shop/product/domain/ProductCategoryRepositoryImpl.java
@@ -1,22 +1,15 @@
 package pl.wit.shop.product.domain;
 
 import lombok.Getter;
-import pl.wit.shop.shared.exception.NotFoundException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.function.Supplier;
 
 public class ProductCategoryRepositoryImpl implements ProductCategoryRepository {
 
     @Getter
     @PersistenceContext
     private EntityManager entityManager;
-
-    @Override
-    public Supplier<NotFoundException> notFoundException(String cause) {
-        return () -> new NotFoundException("");
-    }
 
     @Override
     public ProductCategory getByName(String name) {
