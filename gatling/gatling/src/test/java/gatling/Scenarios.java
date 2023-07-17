4f90afa921db779086cfbac4903c8ddac9fd0ba3
szachMati <mateusz.hawryluk30@gmail.com>
2023-07-17 22:53:12 +0200

    [Gatling] constantUsersPerSec(200)

--- gatling/gatling/src/test/java/gatling/Scenarios.java
+++ gatling/gatling/src/test/java/gatling/Scenarios.java
@@ -10,23 +10,20 @@ import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
 import static io.gatling.javaapi.core.CoreDsl.rampUsersPerSec;
 
 class Scenarios {
-
     static PopulationBuilder peakScenario(ScenarioBuilder scenario) {
         return scenario.injectOpen(
                 constantUsersPerSec(200).during(Duration.ofSeconds(30)),
                 atOnceUsers(2000),
-                constantUsersPerSec(150).during(Duration.ofSeconds(30)),
+                constantUsersPerSec(200).during(Duration.ofSeconds(30)),
                 atOnceUsers(2000),
                 constantUsersPerSec(200).during(Duration.ofSeconds(30))
         );
     }
-
     static PopulationBuilder enduranceScenario(ScenarioBuilder scenario) {
         return scenario.injectOpen(
                 constantUsersPerSec(200).during(Duration.ofMinutes(5))
         );
     }
-
     static PopulationBuilder rampUpScenario(ScenarioBuilder scenario) {
         return scenario.injectOpen(
                 rampUsersPerSec(5).to(1000).during(Duration.ofMinutes(2))
